77e9041efbcb2ffab132d7e72da17e0d12f9fdf1
Damian <G00419511@atu.ie>
2023-10-17 13:53:03 +0100

    Created a new class and added displayInfo

---
+++ src/main/java/ie/atu/Person.java
@@ -0,0 +1,11 @@
+package ie.atu;
+
+public class Person {
+    String firstname;
+    String lastname;
+    int age;
+
+    public void displayInfo(){
+        System.out.println("Name: " + firstname + " " + lastname + ", Age: " + age);
+    }
+}
