dfaee693de5a6da2798f8c9734b9aa03f96d3d97
Damian <G00419511@atu.ie>
2023-10-17 13:59:31 +0100

    Added a Constructor

--- src/main/java/ie/atu/Person.java
+++ src/main/java/ie/atu/Person.java
@@ -5,6 +5,13 @@ public class Person {
     String lastname;
     int age;
 
+    //Constructor
+    public Person(String firstname, String lastname, int age){
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.age = age;
+    }
+
     public void displayInfo(){
         System.out.println("Name: " + firstname + " " + lastname + ", Age: " + age);
     }
